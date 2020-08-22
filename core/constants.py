143dd96fe2b8a441fce4c7157cf18533dca7aa6f
jayruin <28711665+jayruin@users.noreply.github.com>
2020-08-22 15:37:19 -0400

    Move html and epub directories to core.constants.

--- core/constants.py
+++ core/constants.py
@@ -1,10 +1,11 @@
 import datetime
-from typing import List
 
 
 INDENT: str = " " * 4
 BUILD_TIME: str = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
 COVER_XHTML: str = "_cover.xhtml"
+EPUB_DIRECTORY: str = "./epub"
+HTML_DIRECTORY: str = "./html"
 METADATA_JSON: str = "_metadata.json"
 NAV_JSON: str = "_nav.json"
 NAV_XHTML: str = "_nav.xhtml"
--- templates/simple/__main__.py
+++ templates/simple/__main__.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
+from core import constants
 from templates.simple._builder import SimpleBuilder
 
 
 for arg in sys.argv[1:]:
     template_dir = "./templates/simple/contents"
-    src = os.path.join("./html", arg)
-    dst = os.path.join("./epub", arg)
+    src = os.path.join(constants.HTML_DIRECTORY, arg)
+    dst = os.path.join(constants.EPUB_DIRECTORY, arg)
     pb = SimpleBuilder(src, dst, template_dir)
     pb.build()
