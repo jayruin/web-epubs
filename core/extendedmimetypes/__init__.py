4a0786e32e46a7a626631248a91a75c24ec0cea5
jayruin <28711665+jayruin@users.noreply.github.com>
2020-10-30 21:07:06 -0400

    Fix extendedmimetypes.

--- core/extendedmimetypes/__init__.py
+++ core/extendedmimetypes/__init__.py
@@ -2,10 +2,10 @@ import json
 import mimetypes
 import os
 
-with os.scandir("./") as it:
+with os.scandir("./core/extendedmimetypes/") as it:
     for entry in it:
         if entry.is_file() and entry.name.endswith(".json"):
             with open(entry, "r") as f:
                 mapping = json.loads(f.read())
                 for file_ext, file_type in mapping.items():
-                    mimetypes.add_type(file_ext, file_type)
+                    mimetypes.add_type(file_type, file_ext)
