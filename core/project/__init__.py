b36e450eec85662879d84b6cb7693613257dffeb
jayruin <28711665+jayruin@users.noreply.github.com>
2021-09-18 15:57:06 -0400

    Rewrite: Add depth first traversal for tree.

--- core/project/__init__.py
+++ core/project/__init__.py
@@ -3,10 +3,11 @@ from .epub_project import EPUBProject
 from .epub_resource import EPUBResource
 from .epub_resource_manager import EPUBResourceManager
 from .epub_metadata import EPUBMetadata
-from .tree import Tree
+from .tree import depth_first_traversal, Tree
 
 __all__ = [
     "Anchor",
+    "depth_first_traversal",
     "EPUBProject",
     "EPUBResource",
     "EPUBResourceManager",
--- core/project/tree.py
+++ core/project/tree.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Generic, TypeVar
+from typing import Generic, Iterable, TypeVar
 
 T = TypeVar("T")
+
+
+def depth_first_traversal(tree: Tree[T]) -> Iterable[T]:
+    yield tree.value
+    for child in tree.children:
+        yield from depth_first_traversal(child)
 
 
 @dataclass
