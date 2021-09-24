d45f879a4a4b22887cc5a19ac9a8767185064979
jayruin <28711665+jayruin@users.noreply.github.com>
2021-09-24 11:14:07 -0400

    Rewrite: Implement epub.check.

--- core/runner.py
+++ core/runner.py
@@ -1,42 +1,45 @@
-from argparse import ArgumentParser
+import argparse
 import os
-from pathlib import Path
+import pathlib
+import subprocess
 import sys
 
+from core.constants import Encoding
 
-def make_project_argparser(description: str) -> ArgumentParser:
-    cwd = Path(os.getcwd())
-    script = Path(sys.argv[0])
+
+def make_project_argparser(description: str) -> argparse.ArgumentParser:
+    cwd = pathlib.Path(os.getcwd())
+    script = pathlib.Path(sys.argv[0])
     module_parts = script.relative_to(cwd).with_suffix("").parts
     module = ".".join(module_parts)
-    parser = ArgumentParser(
+    parser = argparse.ArgumentParser(
         prog=f"python -m {module}",
         description=description
     )
 
     parser.add_argument(
         "--projects-directory",
-        type=Path
+        type=pathlib.Path
     )
     parser.add_argument(
         "--expanded_epubs_directory",
-        type=Path
+        type=pathlib.Path
     )
     parser.add_argument(
         "--packaged_epubs_directory",
-        type=Path
+        type=pathlib.Path
     )
     parser.add_argument(
         "--epubcheck-directory",
-        type=Path
+        type=pathlib.Path
     )
     parser.add_argument(
         "--logs-directory",
-        type=Path
+        type=pathlib.Path
     )
     parser.add_argument(
         "--bundles-directory",
-        type=Path
+        type=pathlib.Path
     )
 
     parser.add_argument(
@@ -57,3 +60,14 @@ def make_project_argparser(description: str) -> ArgumentParser:
     )
 
     return parser
+
+
+def subprocess_run(args: list[str]) -> str:
+    completed_process = subprocess.run(
+        args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding=Encoding.UTF_8.value
+    )
+    completed_process.check_returncode()
+    return completed_process.stdout
