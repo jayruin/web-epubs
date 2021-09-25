e75e148b559db9fa327203a22f2af1dfe977ac7f
jayruin <28711665+jayruin@users.noreply.github.com>
2021-09-25 10:59:34 -0400

    Implement PackageDocument for epub2.

--- core/documents/package_document.py
+++ core/documents/package_document.py
@@ -1,11 +1,12 @@
 from pathlib import Path
+from typing import Optional
 
 from lxml import etree
 
 from .epub3document import EPUB3Document
 from .epub2document import EPUB2Document
 from core.constants import BUILD_TIME, Namespace
-from core.project import EPUBMetadata, EPUBResource
+from core.project import Anchor, EPUBMetadata, EPUBResource
 from core.serialize import write_xml_element
 
 
@@ -43,7 +44,36 @@ class EPUB2PackageDocument(EPUB2Document):
     """
     An XML document corresponding to the EPUB Package Document.
     """
-    pass
+    UNIQUE_IDENTIFIER_ID = "publication-id"
+
+    def __init__(
+        self,
+        epub_metadata: EPUBMetadata,
+        resources: dict[Path, EPUBResource],
+        progression: list[Path],
+        toc: Path,
+        landmarks: Optional[list[Anchor]] = None
+    ) -> None:
+        self.epub_metadata = epub_metadata
+        self.resources = resources
+        self.progression = progression
+        self.toc = toc
+        self.landmarks = landmarks
+
+    def epub2(self, path: Path) -> None:
+        """
+        http://idpf.org/epub/20/spec/OPF_2.0.1_draft.htm#Section2.0
+        """
+        package = make_epub2_package_element(
+            self.epub_metadata,
+            self.resources,
+            self.progression,
+            self.toc,
+            self.landmarks,
+            self.UNIQUE_IDENTIFIER_ID
+        )
+
+        write_xml_element(package, path)
 
 
 def make_epub3_package_element(
@@ -200,3 +230,156 @@ def make_epub3_spine_element(
         spine.append(itemref)
 
     return spine
+
+
+def make_epub2_package_element(
+    epub_metadata: EPUBMetadata,
+    resources: dict[Path, EPUBResource],
+    progression: list[Path],
+    toc: Path,
+    landmarks: Optional[list[Anchor]],
+    UNIQUE_IDENTIFIER_ID: str
+) -> etree._Element:
+    package = etree.Element(
+        "package",
+        attrib={
+            "unique-identifier": UNIQUE_IDENTIFIER_ID,
+            "version": "2.0"
+        },
+        nsmap={
+            None: Namespace.OPF.value
+        }
+    )
+
+    metadata = make_epub2_metadata_element(
+        epub_metadata,
+        UNIQUE_IDENTIFIER_ID
+    )
+    package.append(metadata)
+
+    manifest = make_epub2_manifest_element(resources)
+    package.append(manifest)
+
+    spine = make_epub2_spine_element(resources, progression, toc)
+    package.append(spine)
+
+    if landmarks is not None:
+        guide = make_epub2_guide_element(landmarks)
+        package.append(guide)
+
+    return package
+
+
+def make_epub2_metadata_element(
+    epub_metadata: EPUBMetadata,
+    UNIQUE_IDENTIFIER_ID: str
+) -> etree._Element:
+    metadata = etree.Element(
+        "metadata",
+        nsmap={
+            "dc": Namespace.DC.value,
+            "opf": Namespace.OPF.value
+        }
+    )
+
+    dc_identifier = etree.Element(
+        etree.QName(Namespace.DC.value, "identifier").text,
+        attrib={
+            "id": UNIQUE_IDENTIFIER_ID
+        }
+    )
+    dc_identifier.text = epub_metadata.identifier
+    metadata.append(dc_identifier)
+
+    for language in epub_metadata.languages:
+        dc_language = etree.Element(
+            etree.QName(Namespace.DC.value, "language").text
+        )
+        dc_language.text = language
+        metadata.append(dc_language)
+
+    dc_title = etree.Element(
+        etree.QName(Namespace.DC.value, "title").text
+    )
+    dc_title.text = epub_metadata.title
+    metadata.append(dc_title)
+
+    for creator, roles in epub_metadata.creators.items():
+        for role in roles:
+            dc_creator = etree.Element(
+                etree.QName(Namespace.DC.value, "creator").text,
+                attrib={
+                    etree.QName(Namespace.OPF.value, "role"): role
+                }
+            )
+            dc_creator.text = creator
+            metadata.append(dc_creator)
+
+    dc_date = etree.Element(etree.QName(Namespace.DC.value, "date").text)
+    dc_date.text = epub_metadata.date
+    metadata.append(dc_date)
+
+    return metadata
+
+
+def make_epub2_manifest_element(
+    resources: dict[Path, EPUBResource]
+) -> etree._Element:
+    manifest = etree.Element("manifest")
+
+    for path in sorted(resources, key=lambda p: p.as_posix()):
+        epub_resource = resources[path]
+        item = etree.Element(
+            "item",
+            attrib={
+                "href": epub_resource.href.as_posix(),
+                "id": epub_resource.manifest_id,
+                "media-type": epub_resource.mimetype
+            }
+        )
+        manifest.append(item)
+
+    return manifest
+
+
+def make_epub2_spine_element(
+    resources: dict[Path, EPUBResource],
+    progression: list[Path],
+    toc: Path
+) -> etree._Element:
+    spine = etree.Element(
+        "spine",
+        attrib={
+            "toc": resources[toc].manifest_id
+        }
+    )
+
+    for path in progression:
+        itemref = etree.Element(
+            "itemref",
+            attrib={
+                "idref": resources[path].manifest_id,
+                "linear": "yes"
+            }
+        )
+        spine.append(itemref)
+
+    return spine
+
+
+def make_epub2_guide_element(landmarks: list[Anchor]) -> etree._Element:
+    guide = etree.Element("guide")
+
+    for anchor in landmarks:
+        assert anchor.type
+        reference = etree.Element(
+            "reference",
+            attrib={
+                "type": anchor.type,
+                "title": anchor.text,
+                "href": anchor.href.as_posix()
+            }
+        )
+        guide.append(reference)
+
+    return guide
