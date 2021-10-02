0b673ada337959dbcee8d15d79258ea3cdb0f92e
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-02 19:04:35 -0400

    Delete old calibre plugin files.

--- plugins/calibre/cli/__init__.py
+++
@@ -1,5 +0,0 @@
-from .calibre import calibre
-from .ebook_viewer import ebook_viewer
-
-
-__all__ = ["calibre", "ebook_viewer"]
--- plugins/calibre/cli/calibredb/__init__.py
+++
@@ -1,6 +0,0 @@
-from .add import add
-from .add_format import add_format
-from .search import search
-
-
-__all__ = ["add", "add_format", "search"]
