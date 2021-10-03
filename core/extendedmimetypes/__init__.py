3a94f426a5ac02d3465bc8e5154eaf2dc8ef6773
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-03 18:30:49 -0400

    core.extendedmimetypes use Path.iterdir and json.load.

--- core/extendedmimetypes/__init__.py
+++ core/extendedmimetypes/__init__.py
@@ -1,12 +1,10 @@
 import json
 import mimetypes
-import os
+from pathlib import Path
 
-
-with os.scandir("./core/extendedmimetypes/") as it:
-    for entry in it:
-        if entry.is_file() and entry.name.endswith(".json"):
-            with open(entry, "r") as f:
-                mapping = json.loads(f.read())
-                for file_ext, file_type in mapping.items():
-                    mimetypes.add_type(file_type, file_ext)
+for entry in Path("core", "extendedmimetypes").iterdir():
+    if entry.is_file() and entry.name.endswith(".json"):
+        with open(entry, "rb") as f:
+            mapping = json.load(f)
+            for file_ext, file_type in mapping.items():
+                mimetypes.add_type(file_type, file_ext)
