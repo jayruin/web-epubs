3fee8a4af3f493d4a5f9f1a1b4654350aa4b9004
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-03 18:26:43 -0400

    Delete legacy constants.

--- core/constants.py
+++ core/constants.py
@@ -45,23 +45,4 @@ EPUBCHECK_RELEASES_URL: str = (
     "https://api.github.com/repos/w3c/epubcheck/releases/latest"
 )
 
-COVER_XHTML: str = "_cover.xhtml"
-EPUB_DIRECTORY: str = "./epub"
-HTML_DIRECTORY: str = "./html"
-METADATA_JSON: str = "_metadata.json"
-NAV_JSON: str = "_nav.json"
-NAV_XHTML: str = "_nav.xhtml"
-PACKAGE_OPF: str = "package.opf"
-ROOT_PATH_DIR: str = "OEBPS"
-TEMPLATE_XHTML: str = "_template.xhtml"
-TOC_NCX: str = "toc.ncx"
-TOC_XHTML: str = "_toc.xhtml"
-
-EPUBCHECK_DOWNLOAD_URL: str = (
-    "https://api.github.com/repos/w3c/epubcheck/releases/latest"
-)
-EPUBCHECK_LOCAL_ZIP = "./epubcheck.zip"
-EPUBCHECK_ROOT_DIR = "./epubcheck"
-EPUBCHECK_SUMMARY_JSON = "./epubcheck.summary.json"
-
 JAVA_EXECUTABLE = "java"
