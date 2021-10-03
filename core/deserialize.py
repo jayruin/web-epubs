98c0068c59692bc9903f8dc3ec00c984f896dd15
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-03 18:21:49 -0400

    Add YAML support.

--- core/deserialize.py
+++ core/deserialize.py
@@ -1,11 +1,29 @@
 from __future__ import annotations
+from collections.abc import Callable
 import json
 from pathlib import Path
-from typing import TypedDict
+from typing import Any, cast, TypedDict, TypeVar
+import yaml
 
 from core.project.anchor import Anchor
 from core.project.epub_metadata import EPUBMetadata
 from core.project.tree import Tree
+
+
+_T = TypeVar("_T")
+
+
+def read_any(
+    path: Path,
+    read_function: Callable[[Path], _T],
+    suffixes: list[str]
+) -> _T:
+    for suffix in suffixes:
+        try:
+            return read_function(path.with_suffix(suffix))
+        except FileNotFoundError:
+            continue
+    raise FileNotFoundError
 
 
 class NavDict(TypedDict):
@@ -22,10 +40,22 @@ def nav_dict_to_tree(data: NavDict) -> Tree[Anchor]:
 
 
 def read_epub_metadata(path: Path) -> EPUBMetadata:
+    if path.suffix == ".json":
+        load = cast(Any, json).load
+    elif path.suffix in [".yaml", ".yml"]:
+        load = cast(Any, yaml).load
+    else:
+        raise ValueError(f"{path.suffix} is unsupported for metadata!")
     with open(path, "rb") as f:
-        return EPUBMetadata(**json.load(f))
+        return EPUBMetadata(**load(f))
 
 
 def read_nav(path: Path) -> list[Tree[Anchor]]:
+    if path.suffix == ".json":
+        load = cast(Any, json).load
+    elif path.suffix in [".yaml", ".yml"]:
+        load = cast(Any, yaml).load
+    else:
+        raise ValueError(f"{path.suffix} is unsupported for nav!")
     with open(path, "rb") as f:
-        return [nav_dict_to_tree(nav_dict) for nav_dict in json.load(f)]
+        return [nav_dict_to_tree(nav_dict) for nav_dict in load(f)]
--- core/project/epub_project.py
+++ core/project/epub_project.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from .anchor import Anchor
 from .epub_metadata import EPUBMetadata
 from .tree import Tree
-from core.deserialize import read_epub_metadata, read_nav
+from core.deserialize import read_any, read_epub_metadata, read_nav
 
 
 class EPUBProject:
@@ -18,15 +18,20 @@ class EPUBProject:
     NCX_DOCUMENT: str = "_toc.ncx"
     PACKAGE_DOCUMENT: str = "_package.opf"
     RESOURCES_DIRECTORY: str = "OEBPS"
+    SUPPORTED_SUFFIXES: list[str] = [".json", ".yaml", ".yml"]
 
     def __init__(self, root: Path) -> None:
         self.root: Path = root
         self.name: str = root.stem
 
-        self.epub_metadata: EPUBMetadata = read_epub_metadata(
-            Path(self.root, self.METADATA_JSON)
+        self.epub_metadata: EPUBMetadata = read_any(
+            Path(self.root, self.METADATA_JSON),
+            read_epub_metadata,
+            self.SUPPORTED_SUFFIXES
         )
 
-        self.nav_trees: list[Tree[Anchor]] = read_nav(
-            Path(self.root, self.NAV_JSON)
+        self.nav_trees: list[Tree[Anchor]] = read_any(
+            Path(self.root, self.NAV_JSON),
+            read_nav,
+            self.SUPPORTED_SUFFIXES
         )
