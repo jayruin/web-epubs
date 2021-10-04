15e9b543d3c0ecfba2e9eeff4945e5629e5bb39c
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-04 12:37:21 -0400

    Add converter.

--- core/legacy/convert.py
+++ core/legacy/convert.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+import json
 from pathlib import Path
 
 from .nav_node import NavNode
@@ -13,3 +15,22 @@ def navnode_to_tree(navnode: NavNode, root_dir: str) -> Tree[Anchor]:
         navnode_to_tree(child, root_dir) for child in navnode.children
     ]
     return Tree[Anchor](anchor, children)
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument("--old", type=Path, required=True)
+    parser.add_argument("--new", type=Path, required=True)
+    args = parser.parse_args()
+    with open(args.old, "rb") as f:
+        nav_nodes = [NavNode.from_dict(d) for d in json.load(f)]
+    nav_trees = [
+        navnode_to_tree(nav_node, args.old.parent)
+        for nav_node in nav_nodes
+    ]
+    with open(args.new, "w", encoding="utf-8") as f:
+        json.dump(nav_trees, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    main()
