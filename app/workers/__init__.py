71120b63d00390b1d0e3abdb9e92de50a572c0ed
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-08 13:14:21 -0400

    Add app.workers.builder.

--- app/workers/__init__.py
+++ app/workers/__init__.py
@@ -1,6 +1,7 @@
 from .app_worker import AppWorker
+from .builder import Builder
 from .checker import Checker
 from .installer import Installer
 from .packer import Packer
 
-__all__ = ["AppWorker", "Checker", "Installer", "Packer"]
+__all__ = ["AppWorker", "Builder", "Checker", "Installer", "Packer"]
---
+++ app/workers/builder.py
@@ -0,0 +1,48 @@
+from typing import Any, Optional
+
+from more_itertools import consume
+
+from .app_worker import AppWorker
+from .build_jobs import BaseBuildJob, EPUB2BuildJob, EPUB3BuildJob, EPUBType
+from app.settings import Settings
+from core.runner import pool_run
+
+
+class Builder(AppWorker):
+    def build_projects(
+        self,
+        projects: list[str],
+        project_type: str,
+        bundles: list[str]
+    ) -> None:
+        args_collection: list[tuple[Settings, str, EPUBType, list[str]]] = []
+        kwargs_collection: list[dict[str, Any]] = []
+        for project in projects:
+            args_collection.append(
+                (self.settings, project, EPUBType(project_type), bundles)
+            )
+            kwargs_collection.append({})
+        consume(
+            pool_run(
+                build_epub,
+                args_collection,
+                kwargs_collection,
+                "process",
+                show_progress=True
+            )
+        )
+
+
+def build_epub(
+    settings: Settings,
+    project: str,
+    epub_type: EPUBType,
+    bundles: list[str]
+) -> None:
+    build_job_types: list[type[BaseBuildJob]] = [EPUB2BuildJob, EPUB3BuildJob]
+    build_job: Optional[BaseBuildJob] = None
+    for build_job_type in build_job_types:
+        if build_job_type.epub_type is epub_type:
+            build_job = build_job_type(settings, project, bundles)
+    assert build_job is not None
+    build_job.run()
