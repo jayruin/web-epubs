f369a1e7d942282a11bf2835d10b9d27f98c9874
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-10 21:50:54 -0400

    Add plugins.pagimg arrange.

---
+++ plugins/pagimg/__main__.py
@@ -0,0 +1,38 @@
+from argparse import Namespace
+from pathlib import Path
+
+from .arrange import arrange
+from app import make_main_argparser
+
+
+def parse_args() -> Namespace:
+    description = "Tools for paginated images"
+    parser = make_main_argparser(description)
+    subparsers = parser.add_subparsers(dest="subparser")
+
+    arrange_parser = subparsers.add_parser(
+        "arrange",
+        description="Arrange files"
+    )
+    arrange_parser.add_argument(
+        "-s", "--suffixes",
+        nargs="*"
+    )
+    arrange_parser.add_argument("directory", type=Path)
+    arrange_parser.set_defaults(run=arrange_from_args)
+
+    args = parser.parse_args()
+    return args
+
+
+def arrange_from_args(args: Namespace) -> None:
+    arrange(args.directory, args.suffixes)
+
+
+def main() -> None:
+    args = parse_args()
+    args.run(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ plugins/pagimg/arrange.py
@@ -0,0 +1,45 @@
+from contextlib import ExitStack
+import math
+from pathlib import Path
+import shutil
+import tempfile
+from typing import Optional
+
+
+def arrange(directory: Path, suffixes: Optional[list[str]] = None) -> None:
+    assert directory.is_dir()
+    files: list[Path] = []
+    subdirectories: list[Path] = []
+    for path in directory.iterdir():
+        if path.is_file():
+            if suffixes is None or path.suffix in suffixes:
+                files.append(path)
+        elif path.is_dir():
+            subdirectories.append(path)
+    try:
+        sorted_files = sorted(files, key=lambda page: int(page.stem))
+    except ValueError:
+        sorted_files = sorted(files, key=lambda page: page.stem)
+    if len(sorted_files) > 0:
+        digits = math.floor(math.log(len(sorted_files), 10)) + 1
+        with ExitStack() as stack:
+            temporary_files = [
+                stack.enter_context(tempfile.TemporaryFile())
+                for _ in range(len(sorted_files))
+            ]
+            for index, file in enumerate(sorted_files):
+                with open(file, "rb") as f:
+                    shutil.copyfileobj(f, temporary_files[index])
+                temporary_files[index].seek(0)
+            pages = [
+                file.with_stem(str(index + 1).zfill(digits))
+                for index, file in enumerate(sorted_files)
+            ]
+            for index, page in enumerate(pages):
+                with open(page, "wb") as f:
+                    shutil.copyfileobj(temporary_files[index], f)
+            files_to_delete = set(files) - set(pages)
+            for file_to_delete in files_to_delete:
+                file_to_delete.unlink()
+    for subdirectory in subdirectories:
+        arrange(subdirectory)
