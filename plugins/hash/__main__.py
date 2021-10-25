8c9417df9aaf04901d192da84d1594cc6baa8efe
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-25 14:42:03 -0400

    Add plugins.hash check.

--- plugins/hash/__main__.py
+++ plugins/hash/__main__.py
@@ -1,10 +1,26 @@
 from argparse import ArgumentParser, Namespace
+import logging
 from pathlib import Path
+from typing import Optional
 
-from .path_hash import create_path_hash
+from .path_hash import check_path_hash, create_path_hash
 from app import make_main_argparser
 from core.constants import Encoding
+from core.deserialize import get_load
 from core.serialize import get_dump
+
+
+def make_logger(log_file: Optional[Path] = None) -> logging.Logger:
+    logger = logging.getLogger("hash")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    if log_file is not None:
+        file_handler = logging.FileHandler(
+            log_file,
+            encoding=Encoding.UTF_8.value
+        )
+        logger.addHandler(file_handler)
+    return logger
 
 
 def parse_args() -> Namespace:
@@ -33,6 +49,17 @@ def parse_args() -> Namespace:
     )
     create_parser.set_defaults(run=create_from_args)
 
+    check_parser = subparsers.add_parser(
+        "check",
+        description="Check hashes",
+        parents=[parent_parser]
+    )
+    check_parser.add_argument(
+        "--log-file",
+        type=Path
+    )
+    check_parser.set_defaults(run=check_from_args)
+
     args = parser.parse_args()
     return args
 
@@ -42,6 +69,14 @@ def create_from_args(args: Namespace) -> None:
     dump = get_dump(args.hashes.suffix)
     with open(args.hashes, "w", encoding=Encoding.UTF_8.value) as f:
         dump(path_hash, f)
+
+
+def check_from_args(args: Namespace) -> None:
+    logger = make_logger(args.log_file)
+    load = get_load(args.hashes.suffix)
+    with open(args.hashes, "rb") as f:
+        expected_path_hash = load(f)
+    check_path_hash(args.path, expected_path_hash, logger)
 
 
 def main() -> None:
--- plugins/hash/path_hash.py
+++ plugins/hash/path_hash.py
@@ -1,12 +1,48 @@
 from collections.abc import Iterable, Mapping
+import hmac
+from logging import Logger
 from pathlib import Path
-from typing import Optional, Union
+from typing import cast, Optional, Union
 
 from .multi_hash import MultiHash
 
 PathHash = Union[Mapping[str, str], Mapping[str, "PathHash"]]
 
 CHUNK_SIZE = 2 ** 16
+
+
+def check_path_hash(
+    path: Path,
+    expected_path_hash: PathHash,
+    logger: Logger
+) -> None:
+    types = set(type(value) for value in expected_path_hash.values())
+    is_terminal = len(types) == 1 and str is types.pop()
+    try:
+        if is_terminal:
+            mismatched_algorithms: list[str] = []
+            actual_path_hash = create_path_hash(
+                path,
+                expected_path_hash.keys()
+            )
+            for algorithm, expected_hexdigest in expected_path_hash.items():
+                expected_hexdigest = cast(str, expected_hexdigest)
+                actual_hexdigest = cast(str, actual_path_hash[algorithm])
+                if not hmac.compare_digest(
+                    expected_hexdigest,
+                    actual_hexdigest
+                ):
+                    mismatched_algorithms.append(algorithm)
+            if len(mismatched_algorithms) > 0:
+                fails = ",".join(mismatched_algorithms)
+                logger.error(f"Path {str(path)}: hashes {fails} do not match")
+        else:
+            for key, value in expected_path_hash.items():
+                child_path = Path(path, key)
+                child_expected_path_hash = cast(dict[str, PathHash], value)
+                check_path_hash(child_path, child_expected_path_hash, logger)
+    except FileNotFoundError:
+        logger.error(f"Path {str(path)} not found")
 
 
 def create_path_hash(
@@ -24,4 +60,4 @@ def create_path_hash(
             child_path.name: create_path_hash(child_path, algorithms)
             for child_path in path.iterdir()
         }
-    return {}
+    raise FileNotFoundError(f"Path {str(path)} does not exist!")
