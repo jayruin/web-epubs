4ced847562a449e5e6c204162a712ef7165208de
jayruin <28711665+jayruin@users.noreply.github.com>
2021-10-26 18:42:18 -0400

    Add plugins.pdf stubs. Waiting for Python 3.10 plugin wheels.

---
+++ plugins/pdf/README.md
@@ -0,0 +1,12 @@
+# python -m plugins.pdf *COMMAND*
+
+Tools for working with PDF files.
+
+## from-pagimg
+
+See `plugins.pagimg` for more details. Convert pagimg project to PDF file.
+
+Args:
+
+* `directory` (positional): path to the directory to process.
+* `pdf_file` (positional): path to the PDF file to output.
---
+++ plugins/pdf/__main__.py
@@ -0,0 +1,34 @@
+from argparse import Namespace
+from pathlib import Path
+
+from app import make_main_argparser
+
+
+def parse_args() -> Namespace:
+    description = "Working with PDF files"
+    parser = make_main_argparser(description)
+    subparsers = parser.add_subparsers(dest="subparser")
+
+    pagimg_parser = subparsers.add_parser(
+        "from-pagimg",
+        description="Convert pagimg project to PDF"
+    )
+    pagimg_parser.add_argument("directory", type=Path)
+    pagimg_parser.add_argument("pdf_file", type=Path)
+    pagimg_parser.set_defaults(run=from_pagimg_from_args)
+
+    args = parser.parse_args()
+    return args
+
+
+def from_pagimg_from_args(args: Namespace) -> None:
+    pass
+
+
+def main() -> None:
+    args = parse_args()
+    args.run(args)
+
+
+if __name__ == "__main__":
+    main()
