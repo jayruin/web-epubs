4172b482f892f2653a615d6395f2d3ed2eebc144
jayruin <28711665+jayruin@users.noreply.github.com>
2021-11-18 09:28:59 -0500

    Remove deleted imports.

--- core/documents/__init__.py
+++ core/documents/__init__.py
@@ -1,7 +1,5 @@
 from .container_xml import ContainerXML
 from .cover_xhtml import CoverXHTML
-from .epub2document import EPUB2Document
-from .epub3document import EPUB3Document
 from .mimetype_file import MimetypeFile
 from .navigation_document import NavigationDocument
 from .ncx_document import NCXDocument
@@ -11,8 +9,6 @@ from .paginated_image import PaginatedImage
 __all__ = [
     "ContainerXML",
     "CoverXHTML",
-    "EPUB2Document",
-    "EPUB3Document",
     "EPUB2PackageDocument",
     "EPUB3PackageDocument",
     "MimetypeFile",
