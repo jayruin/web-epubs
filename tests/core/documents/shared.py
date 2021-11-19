3633a7f1cf22da0ae2b924e31f0e1205950ee397
jayruin <28711665+jayruin@users.noreply.github.com>
2021-11-19 09:03:33 -0500

    Use difflib in tests.

--- tests/core/documents/shared.py
+++ tests/core/documents/shared.py
@@ -1,20 +1,36 @@
+import difflib
 from pathlib import Path
+import sys
 
+from core.constants import Encoding
 from core.documents.abcs import EPUB3Document, EPUB2Document
 from tests.temp import get_temporary_file
+
+
+def compare_files(actual: Path, expected: Path) -> bool:
+    with open(actual, "r", encoding=Encoding.UTF_8.value) as f:
+        actual_lines = f.readlines()
+    with open(expected, "r", encoding=Encoding.UTF_8.value) as f:
+        expected_lines = f.readlines()
+    delta = difflib.unified_diff(
+        actual_lines,
+        expected_lines,
+        fromfile="actual",
+        tofile="expected"
+    )
+    sys.stderr.writelines(delta)
+    actual_bytes = actual.read_bytes()
+    expected_bytes = expected.read_bytes()
+    return actual_bytes == expected_bytes
 
 
 def check_epub2_document(document: EPUB2Document, expected_file: Path) -> bool:
     with get_temporary_file() as actual_file:
         document.write_epub2(actual_file)
-        actual = actual_file.read_bytes()
-        expected = expected_file.read_bytes()
-        return actual == expected
+        return compare_files(actual_file, expected_file)
 
 
 def check_epub3_document(document: EPUB3Document, expected_file: Path) -> bool:
     with get_temporary_file() as actual_file:
         document.write_epub3(actual_file)
-        actual = actual_file.read_bytes()
-        expected = expected_file.read_bytes()
-        return actual == expected
+        return compare_files(actual_file, expected_file)
